c942d5bfc2854244059c73c2a23aa5129bc04dbc
mrunali-26 <mrunaliramteke26@gmail.com>
2024-11-06 20:12:11 +0530

    refactor package name

--- StringCalculator/src/main/java/com/tdd/StringCalculator.java
+++ StringCalculator/src/main/java/com/tdd/StringCalculator.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.tdd;
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.length() < 2) {
--- StringCalculator/src/test/java/com/tdd/StringCalculatorTest.java
+++ StringCalculator/src/test/java/com/tdd/StringCalculatorTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.tdd;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
